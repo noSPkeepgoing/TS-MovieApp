74489751cf627cab04a28315e38e63baa896efa5
sunpaaaa <sunpaaaa@gmail.com>
2023-08-08 18:23:40 +0900

    Show movie's title list on MovieList componenet using OMDb API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import { Component } from '../core/sunpaaaa';
-
+import movieStore, { searchMovies } from '../store/movie';
 export default class Search extends Component {
   render() {
     this.el.classList.add('search');
@@ -11,13 +11,20 @@ export default class Search extends Component {
         `;
 
     const inputEl = this.el.querySelector('input');
-    inputEl.addEventListener('input', () => {});
+    inputEl.addEventListener('input', () => {
+      movieStore.state.searchText = inputEl.value;
+    });
     inputEl.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter') {
+      if (event.key === 'Enter' && movieStore.state.searchText.trim()) {
+        searchMovies(1);
       }
     });
 
-    const btnEl = this.el.querySelector('button');
-    btnEl.addEventListener('click', () => {});
+    const btnEl = this.el.querySelector('.btn');
+    btnEl.addEventListener('click', () => {
+      if (movieStore.state.searchText.trim()) {
+        searchMovies(1);
+      }
+    });
   }
 }
