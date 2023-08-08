56f0fd024dd4cf1149faeaf38f57aea4449939a4
sunpaaaa <sunpaaaa@gmail.com>
2023-08-08 12:37:04 +0900

    Add Headline component to Home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,11 @@
 import { Component } from '../core/sunpaaaa';
+import Headline from '../components/Headline';
 
 export default class Home extends Component {
   render() {
-    this.el.innerHTML = '<h1>Hello World!</<h1>';
+    const headline = new Headline().el;
+
+    this.el.classList.add('container');
+    this.el.append(headline);
   }
 }
