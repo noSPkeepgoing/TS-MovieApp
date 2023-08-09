8ea035c6548554a25b40b06f40daa98c9f2f3a46
sunpaaaa <sunpaaaa@gmail.com>
2023-08-09 12:23:01 +0900

    Add movie detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,8 @@
 import { createRouter } from '../core/sunpaaaa';
 import Home from './Home';
+import Movie from './Movie';
 
-export default createRouter([{ path: '#/', component: Home }]);
+export default createRouter([
+  { path: '#/', component: Home },
+  { path: '#/movie', component: Movie },
+]);
