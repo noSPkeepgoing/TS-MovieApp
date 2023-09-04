a245e9150fe8e6fdccd8e483a3e03a4669300bfd
sunpaaaa <sunpaaaa@gmail.com>
2023-09-04 19:10:12 +0900

    Compiled Typescript

--- src/core/sunpaaaa.ts
+++ src/core/sunpaaaa.ts
@@ -1,10 +1,19 @@
 // Component
+interface ComponentPayload {
+  tagName?: string;
+  props?: { [key: string]: unknown };
+  state?: { [key: string]: unknown };
+}
+
 export class Component {
-  constructor(payload = {}) {
+  public el;
+  public state;
+  public props;
+  constructor(payload: ComponentPayload = {}) {
     const {
       tagName = 'div', //
-      state = {}, //
       props = {}, //
+      state = {}, //
     } = payload;
     this.el = document.createElement(tagName);
     this.state = state;
@@ -17,8 +26,14 @@ export class Component {
 }
 
 // Router
+interface Route {
+  path: string;
+  component: typeof Component;
+}
 
-function routeRender(routes) {
+type Routes = Route[];
+
+function routeRender(routes: Routes) {
   if (!location.hash) {
     history.replaceState(null, '', '/#/');
   }
@@ -26,25 +41,31 @@ function routeRender(routes) {
   const routerView = document.querySelector('router-view');
   const [hash, queryString = ''] = location.hash.split('?');
 
+  interface Query {
+    [key: string]: string;
+  }
+
   const query = queryString
     .split('&') //
     .reduce((acc, cur) => {
       const [key, value] = cur.split('=');
       acc[key] = value;
       return acc;
-    }, {});
+    }, {} as Query);
   history.replaceState(query, '');
 
   const currentRoute = routes.find((route) =>
     new RegExp(`${route.path}/?$`).test(hash)
   );
-  routerView.innerHTML = '';
-  routerView.append(new currentRoute.component().el);
+  if (routerView) {
+    routerView.innerHTML = '';
+    currentRoute && routerView.append(new currentRoute.component().el);
+  }
 
   window.scrollTo(0, 0);
 }
 
-export function createRouter(routes) {
+export function createRouter(routes: Routes) {
   return function () {
     window.addEventListener('popstate', () => {
       routeRender(routes);
@@ -54,10 +75,18 @@ export function createRouter(routes) {
 }
 
 // Store
-export class Store {
-  constructor(state) {
-    this.state = {};
-    this.observers = {};
+interface StoreObservers {
+  [key: string]: SubscribeCallback[];
+}
+
+interface SubscribeCallback {
+  (arg: unknown): void;
+}
+
+export class Store<S> {
+  public state = {} as S;
+  private observers = {} as StoreObservers;
+  constructor(state: S) {
     for (const key in state) {
       Object.defineProperty(this.state, key, {
         get: () => state[key],
@@ -70,7 +99,7 @@ export class Store {
       });
     }
   }
-  subscribe(key, cb) {
+  subscribe(key: string, cb: SubscribeCallback) {
     Array.isArray(this.observers[key])
       ? this.observers[key].push(cb)
       : (this.observers[key] = [cb]);
